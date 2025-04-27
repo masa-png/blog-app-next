daf843314e65dadb3698dd032ab94593984284bc
三嶋雅幸 <chicks-job-62@icloud.com>
2025-04-27 18:10:45 +0900

    8章課題の実装

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <>
+      <header className="bg-[#333] text-white font-bold p-6 flex justify-between items-center shadow-lg">
+        <Link href="/" className="text-base">
+          Blog
+        </Link>
+        <Link href="/contact" className="text-base">
+          お問い合わせ
+        </Link>
+      </header>
+    </>
+  );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Form from "./_components/Form";
+
+export default function Page() {
+  return (
+    <div>
+      <div className="max-w-3xl mx-auto py-10">
+        <h1 className="text-xl font-bold mb-10">問い合わせフォーム</h1>
+        <Form />
+      </div>
+    </div>
+  );
+}
