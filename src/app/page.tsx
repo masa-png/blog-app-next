5f2c821fef74197e1de53cd026b4ea2df62d478e
三嶋雅幸 <chicks-job-62@icloud.com>
2025-04-30 10:49:31 +0900

    9章課題の実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,24 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { Post } from "./_types";
+import { MicroCmsPost } from "./_types/MicroCmsPost";
 
 export default function Page() {
-  const [posts, setPosts] = useState<Post[]>([]);
+  const [posts, setPosts] = useState<MicroCmsPost[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
 
   // APIでpostsを取得する処理をuseEffectで実行
   useEffect(() => {
     const fetcher = async () => {
       setLoading(true);
-      const res = await fetch(
-        "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts"
-      );
-      const { posts } = await res.json();
-      setPosts(posts);
+      const res = await fetch("https://m10s91bwq0.microcms.io/api/v1/posts", {
+        headers: {
+          "X-MICROCMS-API-KEY": process.env
+            .NEXT_PUBLIC_MICROCMS_API_KEY as string,
+        },
+      });
+      const { contents } = await res.json();
+      setPosts(contents);
       setLoading(false);
     };
 
@@ -32,7 +35,7 @@ export default function Page() {
       <div className="max-w-3xl mx-auto my-10 px-4">
         <ul>
           {/* ブログの記事をループして表示 */}
-          {posts.map((post: Post) => {
+          {posts.map((post: MicroCmsPost) => {
             return (
               <li key={post.id} className="flex flex-col list-none m-0 p-0">
                 <Link href={`posts/${post.id}`} className="block bg-white">
@@ -43,18 +46,16 @@ export default function Page() {
                           {new Date(post.createdAt).toLocaleDateString()}
                         </div>
                         <div className="flex flex-wrap">
-                          {post.categories.map(
-                            (category: string, index: number) => {
-                              return (
-                                <div
-                                  key={index}
-                                  className="px-1.5 py-1 mr-2 text-xs text-blue-600 border border-blue-600 rounded"
-                                >
-                                  {category}
-                                </div>
-                              );
-                            }
-                          )}
+                          {post.categories.map((category) => {
+                            return (
+                              <div
+                                key={category.id}
+                                className="px-1.5 py-1 mr-2 text-xs text-blue-600 border border-blue-600 rounded"
+                              >
+                                {category.name}
+                              </div>
+                            );
+                          })}
                         </div>
                       </div>
                       <p className="text-[#333] text-2xl mb-4 mt-2">
